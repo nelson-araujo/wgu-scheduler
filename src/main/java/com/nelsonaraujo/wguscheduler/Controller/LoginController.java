ec6a6cb2b97b04d875e2cafa41524bfa2c8bd3be
Nelson Araujo <nelson@nelsonaraujo.com>
2022-02-09 09:25:33 -0500

    feat: add user location to login scene

--- src/main/java/com/nelsonaraujo/wguscheduler/Controller/LoginController.java
+++ src/main/java/com/nelsonaraujo/wguscheduler/Controller/LoginController.java
@@ -1,12 +1,16 @@
 package com.nelsonaraujo.wguscheduler.Controller;
 
 import com.nelsonaraujo.wguscheduler.Model.DBConnection;
+import com.nelsonaraujo.wguscheduler.Model.UserLocale;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,10 +20,13 @@ public class LoginController implements Initializable {
     @FXML private PasswordField passwordField;
     @FXML private Label serverNameLabel;
     @FXML public Label loginErrorLabel;
+    @FXML private ImageView userLocaleImage;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        serverNameLabel.setText(DBConnection.SERVER_NAME);
+        serverNameLabel.setText(DBConnection.SERVER_NAME); // Set server name
+        userLocaleImage.setImage(UserLocale.getLocaleFlag());
+            Tooltip.install(userLocaleImage, new Tooltip(UserLocale.getUserLocaleString()));
     }
 
     @FXML
